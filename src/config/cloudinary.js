aece1e7124a894c113247ee40255fabdc73efbe1
Ilias Almerekov <ilias.almerekov@db-n.com>
2025-09-25 16:43:45 +0200

    fix: cloudinary env to upload images on prod

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,9 +1,28 @@
 import { v2 as cloudinary } from 'cloudinary'
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
+const {
+  CLOUDINARY_URL,
+  CLOUD_NAME,
+  CLOUD_API_KEY,
+  CLOUD_API_SECRET,
+} = process.env;
+
+if (CLOUDINARY_URL) {
+  // Render and other hosted environments usually expose a single CLOUDINARY_URL
+  cloudinary.config({
+    secure: true,
+  });
+} else if (CLOUD_NAME && CLOUD_API_KEY && CLOUD_API_SECRET) {
+  cloudinary.config({
+    cloud_name: CLOUD_NAME,
+    api_key: CLOUD_API_KEY,
+    api_secret: CLOUD_API_SECRET,
+    secure: true,
+  });
+} else {
+  throw new Error(
+    'Missing Cloudinary configuration. Provide CLOUDINARY_URL or CLOUD_NAME/CLOUD_API_KEY/CLOUD_API_SECRET.'
+  );
+}
 
 export default cloudinary;
